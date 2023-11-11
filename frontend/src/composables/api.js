1614c5ae9a52f441548e42261da3d7177e0741bf
Irina <sidoren-irina@yandex.ru>
2023-11-11 12:43:14 +0300

    Add game basic logic

--- frontend/src/composables/api.js
+++ frontend/src/composables/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const apiUrl = import.meta.env.DEV ? 'http://localhost:8080' : 'http://hackathon.twnsnd.online'
+const apiUrl = 'http://hackathon.twnsnd.online'
 
 export default axios.create({
   baseURL: apiUrl,
